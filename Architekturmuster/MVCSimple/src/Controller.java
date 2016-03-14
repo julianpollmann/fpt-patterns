1f390407c7484e8814c58b1f71b72efd3b510e52
julianpollmann <julian.pollmann@gmx.de>
2016-03-14 14:25:04 +0100

    MVCSimple

--- Architekturmuster/MVCSimple/src/Controller.java
+++ Architekturmuster/MVCSimple/src/Controller.java
@@ -1,5 +1,7 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Controller {
+public class Controller implements ActionListener {
 
 	private Model model;
 	private View view;
@@ -11,6 +13,14 @@ public class Controller {
 	public void link(Model model, View view) {
 		this.model = model;
 		this.view = view;
+
+		this.model.addObserver(view);
+		view.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.model.addString(view.getInputPaneText());
 	}
 
 }
--- Architekturmuster/MVCSimple/src/View.java
+++ Architekturmuster/MVCSimple/src/View.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+
 import javafx.collections.FXCollections;
 import javafx.scene.control.ListView;
 
@@ -9,15 +12,25 @@ public class View implements Observer {
 
 	private ArrayList<String> list;
 	private ListView<String> listView;
+	private JButton button;
+	private JTextPane inputpane;
 
 	public View() {
-		// TODO Auto-generated constructor stub
+		button = new JButton();
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		list = ((Model) o).getStrings();
 		listView.setItems(FXCollections.observableArrayList(list));
+	}
+
+	public void addActionListener(Controller controller) {
+		button.addActionListener(controller);
+	}
+
+	public String getInputPaneText() {
+		return inputpane.getText();
 	}
 
 }
